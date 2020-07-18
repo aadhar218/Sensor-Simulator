815382d69d99d9e35573a2e1f0c9200cfdd32acd
aadhar218 <aadharsharma218@gmail.com>
2020-07-18 20:52:08 +0530

    drive driver class

--- climate_sensor.js
+++ climate_sensor.js
@@ -1,96 +1,80 @@
-const express = require('express');
-const app = express();
-var templow=  Math.floor((Math.random() *2) + 1);
-var temphig=  Math.floor((Math.random() * 100) + 1);
-var humilow=  Math.floor((Math.random() *2) + 1);
-var humihig=  Math.floor((Math.random() *100) + 1);
-var tempmax=0x0000
-var humimax=0x0000
-var tempthl=0x0000
-var tempthh=0x00ff
-var rhthrl=0x0000
-var rhthrh=0x00ff
-var measurconfig=0x0000
-var manufidl=0x0049
-var manufidh=0x0054
-var deviidl=0x00d0
-var deviidh=0x0007
-function devgettemp(){
-    templow=  Math.floor((Math.random() *0.1) + 1);
-    temphig=  Math.floor((Math.random() * 10) + 1);
-    return (templow*256+temphig);
-}
-function devgethumi(){
-    return humihig*256+humilow;
-}
-function devgettempmax(){
-    return tempmax;
-}
-function devgethumimax(){
-    return humimax;
-}
-function devgettempthl(){
-    return tempthl;
-}
-function devsettempthl(value){
-    tempthl=value;
-}
 
-function devsettempthh(value){
-    tempthh=value;
-}
-function devgettempthh(){
-    return tempthh;
-}
-function devgetrhthrl(){
-    return rhthrl;
-}
-function devsetrhthrl(value){
-    rhthrl=value;
-}
-function devgetrhthrh(){
-    return rhthrh;
-}
-function devsetrhthrh(value){
-    rhthrh=value;
-}
-function devgetmeasurconfig(){
-    return measurconfig;
-}
-function devsetmeasurconfig(value){
-    measurconfig=value;
-}
-function devgetmanufid(){
-    return manufidh*256+manufidl;
-}
-function devgetdeviid(){
-    return deviidh*256+deviidl;
-}
-function devgettempinc(){
-    return (((temphig*256+templow)/65536)*165)-40;
-}
-function devgethumiinrh(){
-    return ((humihig*256+humilow)/65536)*100;
-}
+class climate_sensor 
+{
+    constructor(){
+        this.templow=  Math.floor((Math.random() *0.02) + 1);
+        this.temphig=  Math.floor((Math.random() * 10) + 1);
+        this.humilow=  Math.floor((Math.random() *0.02) + 1);
+        this.humihig=  Math.floor((Math.random() *10) + 1);
+        this.tempmax=0x0000
+        this.humimax=0x0000
+        this.tempthl=0x0000
+        this.tempthh=0x00ff
+        this.rhthrl=0x0000
+        this.rhthrh=0x00ff
+        this.measurconfig=0x0000
+        this.manufidl=0x0049
+        this.manufidh=0x0054
+        this.deviidl=0x00d0
+        this.deviidh=0x0007
+        this.value = 0
+    }
+    devgettemp(){
+        this.templow=  Math.floor((Math.random() *0.1) + 1);
+        this.temphig=  Math.floor((Math.random() * 10) + 1);
+        return (this.templow*256+this.temphig);
+    }
+    devgethumi(){
+        return this.humihig*256+this.humilow;
+    }
+    devgettempmax(){
+        return this.tempmax;
+    }
+    devgethumimax(){
+        return this.humimax;
+    }
+    devgettempthl(){
+        return this.tempthl;
+    }
+    devsettempthl(){
+        this.tempthl=value;
+    }
 
-module.exports = {
-    devgetdeviid,
-    devgethumi,
-    devgetmanufid,
-    devgettemp,
-    devgethumiinrh,
-    devgettempmax,
-    devgettempinc,
-    devsetmeasurconfig,
-    devgetmeasurconfig,
-    devsetrhthrl,
-    devgetrhthrh,
-    devsetrhthrh,
-    devgetrhthrh,
-    devsettempthl,
-    devsettempthh,
-    devgetrhthrl,
-    devgettempthh,
-    devgettempthl,
-    devgethumimax
+    devsettempthh(){
+        this.tempthh=value;
+    }
+    devgettempthh(){
+        return this.tempthh;
+    }
+    devgetrhthrl(){
+        return this.rhthrl;
+    }
+    devsetrhthrl(){
+        this.rhthrl=value;
+    }
+    devgetrhthrh(){
+        return this.rhthrh;
+    }
+    devsetrhthrh(){
+        this.rhthrh=value;
+    }
+    devgetmeasurconfig(){
+        return this.measurconfig;
+    }
+    devsetmeasurconfig(){
+        this.measurconfig=value;
+    }
+    devgetmanufid(){
+        return this.manufidh*256+this.manufidl;
+    }
+    devgetdeviid(){
+        return this.deviidh*256+this.deviidl;
+    }
+    devgettempinc(){
+        return (((this.temphig*256+this.templow)/65536)*165)-40;
+    }
+    devgethumiinrh(){
+        return ((this.humihig*256+this.humilow)/65536)*100;
+    }
 }
+module.exports = climate_sensor;
